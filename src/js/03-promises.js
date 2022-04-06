1f8f287c8b55bcae4ea0a42c25f3afdb2555d0a1
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-04-06 19:18:15 +0400

    finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onBtnSubmit);
+
+function onBtnSubmit(evn) {
+  evn.preventDefault();
+  const { delay, step, amount } = evn.currentTarget.elements;
+
+  let timedelay = Number(delay.value);
+  for (let counter = 1; counter <= amount.value; counter += 1) {
+    createPromise(counter, timedelay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    timedelay += Number(step.value);
   }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.4;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
